271196b13dc163baec820e2c7778e7045c8c9e35
molodezky <molodezky@ukr.net>
2017-06-30 18:00:23 +0300

    update 0.1

--- templates/molodezky_bootstrap_col-2/source/boxes.php
+++ templates/molodezky_bootstrap_col-2/source/boxes.php
@@ -31,7 +31,7 @@
   }
   if (SET_BOX_LAST_VIEWED == 'true') require(DIR_WS_BOXES . 'last_viewed.php');
   if (substr(basename($PHP_SELF), 0,8) != 'advanced') { if (SET_BOX_WHATSNEW == 'true') require(DIR_WS_BOXES . 'whats_new.php'); }
-  //if (substr(basename($PHP_SELF), 0,8) != 'advanced') { if (SET_BOX_WHATSNEW == 'true') require(DIR_WS_BOXES . 'whats_new_multiproduct.php'); }
+  if (substr(basename($PHP_SELF), 0,8) != 'advanced') { if (SET_BOX_WHATSNEW == 'true') require(DIR_WS_BOXES . 'whats_new_multiproduct.php'); }
   if (SET_BOX_SEARCH == 'true') require(DIR_WS_BOXES . 'search.php');
   if (SET_BOX_CONTENT == 'true') require(DIR_WS_BOXES . 'content.php');
   if (SET_BOX_INFORMATION == 'true') require(DIR_WS_BOXES . 'information.php');
@@ -54,12 +54,12 @@
 
   if (!$product->isProduct()) {
     if (SET_BOX_SPECIALS == 'true') include(DIR_WS_BOXES . 'specials.php');
-    //if (SET_BOX_SPECIALS == 'true') include(DIR_WS_BOXES . 'specials_multiproduct.php');
+    if (SET_BOX_SPECIALS == 'true') include(DIR_WS_BOXES . 'specials_multiproduct.php');
   }
 
   if (!$product->isProduct()) {
     if (SET_BOX_FEATURED == 'true') include(DIR_WS_BOXES . 'featured.php');
-    //if (SET_BOX_FEATURED == 'true') include(DIR_WS_BOXES . 'featured_multiproduct.php');
+    if (SET_BOX_FEATURED == 'true') include(DIR_WS_BOXES . 'featured_multiproduct.php');
   }
 
   if ($_SESSION['customers_status']['customers_status_read_reviews'] == 1) { if (SET_BOX_REVIEWS == 'true') require(DIR_WS_BOXES . 'reviews.php'); }
