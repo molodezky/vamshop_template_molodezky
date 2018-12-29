68c6b3e2fbbdc3adf4acf72d59bfd56c3202432b
molodezky <molodezky@ukr.net>
2018-12-29 10:44:57 +0200

    update 1.95.0

--- templates/molodezky_bootstrap_col-2/css/css.php
+++ templates/molodezky_bootstrap_col-2/css/css.php
@@ -1,2 +1,3 @@
-<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/css/font-awesome.min.css'; ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/css/bootstrap.min.css'; ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/stylesheet.css'; ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/css/font-awesome.min.css'; ?>" />
